680027de63dfd4ee58275aceb393f550d5a31869
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-02-16 20:06:03 +0545

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   base: "/",
   build: {
     outDir: "dist",
